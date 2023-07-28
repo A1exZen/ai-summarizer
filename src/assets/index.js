89f602d3bd081b0e23863694de21c9feb7a7f4b0
Алексей Зеньчик <AlexComp2021@gmail.com>
2023-07-28 12:15:26 +0300

    first commit

---
+++ src/assets/index.js
@@ -0,0 +1,14 @@
+import linkIcon from './link.svg'
+import loader from './loader.svg'
+import copy from './copy.svg'
+import logo from './logo.svg'
+import tick from './tick.svg'
+import iconLogo from './iconLogo.svg'
+export {
+    iconLogo,
+    linkIcon,
+    loader,
+    copy,
+    logo,
+    tick
+}
